a825d8d9007782bee378b3e58c997d0839ba0397
BJMinhNhut <nhutben678@gmail.com>
2023-04-08 14:01:41 +0700

    fix singlynode

--- inc/SinglyNode.hpp
+++ inc/SinglyNode.hpp
@@ -18,6 +18,8 @@ class SinglyNode : public Entity {
     int getValue();
     void setNextNode(SinglyNode* newNode);
 
+   private:
+    virtual void updateCurrent(sf::Time dt);
     virtual void drawCurrent(sf::RenderTarget& target,
                              sf::RenderStates states) const;
 
